fed15b57111d2b62d840f428054279ec5aadb9a8
the-phoenix <9201924+the-phoenix@users.noreply.github.com>
2019-03-06 18:51:06 +0200

    intial commit

---
+++ FSharpAPIBoilerplate/Controllers/ValuesController.fs
@@ -0,0 +1,44 @@
+﻿namespace FSharpAPIBoilerplate.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+
+[<Route("api/[controller]")>]
+[<ApiController>]
+type ValuesController () =
+    inherit ControllerBase()
+
+    let values = [|"value1"; "value2"|]
+
+    [<HttpGet>]
+    member this.Get() =
+        ActionResult<string[]>(values)
+
+    [<HttpGet("{id}")>]
+    member this.Get(id:int) =
+        let res = 
+            values 
+            |> Array.tryItem id
+
+
+        let value = 
+            match res with
+            | Some value -> value
+            | None -> sprintf "No value of index %d" id
+            
+        ActionResult<string>(value)
+
+    [<HttpPost>]
+    member this.Post([<FromBody>] value:string) =
+        ()
+
+    [<HttpPut("{id}")>]
+    member this.Put(id:int, [<FromBody>] value:string ) =
+        ()
+
+    [<HttpDelete("{id}")>]
+    member this.Delete(id:int) =
+        ()
